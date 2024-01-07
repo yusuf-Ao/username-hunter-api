c95ed35af73b29b435d7d0a4757a5fcc8c2c1055
Yusuf <ay.techbuilds@gmail.com>
2024-01-07 16:30:55 +0100

    Updated Algo2

--- src/app.service.ts
+++ src/app.service.ts
@@ -139,9 +139,7 @@ export class AppService {
     result.url = `https://www.instagram.com/${username}`;
     result.verified = false;
     try {
-      const response = await this.httpService
-        .get(result.url, { headers: this.headers })
-        .toPromise();
+      const response = await this.httpService.get(result.url).toPromise();
       if (!response.data) {
         throw new Error('No data received from the URL.');
       }
